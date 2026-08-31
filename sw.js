3f0440063c0d21b764ace691bdd55639837a60a9
handahong <cookrista1227@naver.com>
2026-08-31 13:34:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// 오프라인 캐시. 파일을 고치면 아래 버전 숫자를 올려야 새 버전이 반영돼.
+const V = 'jp-srs-v1';
+const FILES = ['./', './index.html', './manifest.json',
+  './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(V).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
